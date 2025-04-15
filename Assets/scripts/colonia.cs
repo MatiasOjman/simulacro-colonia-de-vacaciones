79a4a14501b88b041fb390b3044a5327896aa723
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2025-04-15 12:06:52 -0300

    1era parte terminada

---
+++ Assets/scripts/colonia.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colonia : MonoBehaviour
+{
+    public int cantidadInfantiles;
+    public int cantidadJuveniles;
+    int profesorJuvenil;
+    int profesorInfantil;
+    int coordinador;
+    int listaEspera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cantidadJuveniles <0 || cantidadJuveniles >100 || cantidadInfantiles < 0 || cantidadInfantiles > 100)
+        {
+            Debug.Log("tus datos no son correctos");
+            return;
+       
+        }
+        profesorInfantil = cantidadInfantiles / 10;
+        Debug.Log("Se necesitan " + profesorInfantil + " profesores para los inscriptos infantiles");
+        profesorJuvenil = cantidadJuveniles / 20;
+        Debug.Log("Se necesitan " + profesorJuvenil + " profesores para los inscriptos juveniles");
+
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
